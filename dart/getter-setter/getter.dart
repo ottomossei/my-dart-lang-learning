0581a0738f47b3129c6569664b5cd02b0f0cdaeb
ottomossei <seki.jobhunting@gmail.com>
2022-01-19 22:11:01 +0900

    Add file

--- dart/getter-setter/getter.dart
+++ dart/getter-setter/getter.dart
@@ -1,3 +1,13 @@
+// getter:参照できるメリット
+// 計算型プロパティを表現できる。
+// final int value;
+// int get alpha => (0xff000000 & value) >> 24;
+// int alphaとかはくどい（他言語なら値をキャッシュして軽くなる）
+// setter:条件付きで値を変更できるメリット
+// 関連ワード：Computed property、関数従属性
+// getter&setter:リファクタリング、クライアント側（main.dart）を
+// 変更せずに、クラス内を変更しやすい
+
 class Car {
   String _name;
   final int _number;
@@ -10,6 +20,14 @@ class Car {
 
   // Setter
   set name(String n) {
-    _name = n;
+    if (n == "Nissan") {
+      _name = n;
+    }
   }
+
+  // Error
+  // final can't set
+  // set number(int num){
+  //   _number = num;
+  // }
 }

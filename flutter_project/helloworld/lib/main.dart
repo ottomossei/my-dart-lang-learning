59d861c8079525041e2aed228d3b83ebdc119d35
ottomossei <seki.jobhunting@gmail.com>
2022-01-22 23:10:55 +0900

    Add AppBar

--- flutter_project/helloworld/lib/main.dart
+++ flutter_project/helloworld/lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Debug"),
+        ),
         backgroundColor: Colors.black,
         body: Container(),
       ),
